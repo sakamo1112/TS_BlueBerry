28ee9db0660912a230f11bc1f97637395bac0f19
sakamo1112 <sakamo78911@gmail.com>
2023-08-08 09:33:39 +0900

    2-1のtraining

--- practice/dist/index.js
+++ practice/dist/index.js
@@ -1,3 +1,13 @@
 "use strict";
 const message = "Hello, World!";
+const greeting = "Hello, ";
+const target = "World!";
 console.log(message);
+console.log(greeting + target);
+const i = 11;
+if (i < 10) {
+    console.log(i);
+}
+else {
+    console.log(i + 10);
+}
