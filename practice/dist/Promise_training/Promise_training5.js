33ffa06ad037d6992ddcd3344a7bad9350fd66aa
坂本海人 <sakamo78911@gmail.com>
2023-12-04 17:28:18 +0900

    Promiseの静的メソッド2-2

--- practice/dist/Promise_training/Promise_training5.js
+++ practice/dist/Promise_training/Promise_training5.js
@@ -1,31 +1,30 @@
-import { readFile } from 'fs/promises';
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var promises_1 = require('fs/promises');
 //Promiseの静的メソッド2-2 .race
 //.raceは最初に解決したPromiseの結果を受け取る（成功か失敗かは関係なく、解決したもの）
 //タイムアウト処理に使える
-
-const sleepReject = (duration: number) => {
-    return new Promise<never>((resolve, reject) => {
+var sleepReject = function (duration) {
+    return new Promise(function (resolve, reject) {
         setTimeout(reject, duration);
     });
 };
-
-const p = Promise.race([
-    readFile('data/foo.txt', 'utf-8').catch((error: unknown) => {
+var p = Promise.race([
+    (0, promises_1.readFile)('data/foo.txt', 'utf-8').catch(function (error) {
         console.log('data/foo.txt');
         console.log(error);
     }),
-    readFile('data/bar.txt', 'utf-8').catch((error: unknown) => {
+    (0, promises_1.readFile)('data/bar.txt', 'utf-8').catch(function (error) {
         console.log('data/bar.txt');
         console.log(error);
     }),
     sleepReject(100),
 ]);
-
 p.then(
-    (result) => {
+    function (result) {
         console.log(result);
     },
-    (error: unknown) => {
+    function (error) {
         console.log('失敗');
         console.log(error);
     }
